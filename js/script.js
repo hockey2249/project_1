1800e8fa10336073208aa5e1e7ae1898f590d349
hockey2249 <kgibbons14@gmail.com>
2016-06-28 08:40:59 -0600

    Added a constructor function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
- var player1 = 0;
- var player2 = 0; 
- var player3 = 0;
+function Question(question, answer) {
+    this.question = question;
+    this.answer = answer;
+}
 
 
- var $answer1 = $("#box1").click(function() {
-  	if (prompt("What is Question #1?") === "Kyle"){
-	    	alert("Correct");
-			document.getElementById("box1").background = "violet";
+
+// var question1 = new Question_answer("What is Kyle's name?", "Kyle");
+
+
+// for (var i = 0, l = questions.length; i < l; i++) {
+//     answer = prompt(questions[i].question);
+// }
+
+// Create a constructor function Object and pass in different questions, question1 = New Question(prompt("ueowfnouwenf"))
+
+// else if (true) {
+	// add 100 or 500 points 
+//}
+// // Currently creating a function for each question. It's prompting a question and then alerting if it is correct or not 
+
+function Game(){
+
+var question1 = new Question("What is Kyle's name?", "Kyle");
+
+var $answer1 = $("#box1").click(function() {
+	if (prompt(this.question1.Question).toUpperCase() === "OBJECT"){
+	    	alert("Correct!");
+			this.style.color = 'blue';
+			document.getElementById('box1').style.pointerEvents = 'none';
 	    	} else {
-	    		alert("NOOOOO");
+	    		alert("That is not the correct answer!");
+	    		this.style.color = 'blue';
+	    		document.getElementById('box1').style.pointerEvents = 'none';
 	    	}});
- var $answer2 = $("#box2").click(function() {
-  	if (prompt("What is Question #2?") === "Kyle"){
-	    		alert("Correct");
+var $answer2 = $("#box2").click(function() {
+	if (prompt("What does a function always have?").toUpperCase() === "NAME" || "STATEMENTS INSIDE THE FUNCTION" || "AN OPTIONAL LIST OF ARGUMENTS") {
+	    	alert("Correct!");
+			this.style.color = 'blue';
+			// changing background color to blue
+			document.getElementById('box2').style.pointerEvents = 'none';
 	    	} else {
-	    		alert("NOOOOO");
-	    	}});
-var $answer3 = $("#box3").click(function() {
- 	if (prompt("What is Question #3?") === "Kyle"){
-	    		alert("Correct");
-	    	} else {
-	    		alert("NOOOOO");
-	    	}});
+	    		alert("That is not the correct answer!");
+	    		this.style.color = 'blue';
+	    		document.getElementById('box2').style.pointerEvents = 'none';
+	    	}});	
+}
+
+// Need to figure out a way to store each input answer
