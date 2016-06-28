8e8cc3ad135871a0335da63b8e27f2bccc03fa40
hockey2249 <kgibbons14@gmail.com>
2016-06-27 20:26:00 -0600

    New Commit for Jeopardy

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Project #1 - Instructions
 Trello Board - Link Below 
 	https://trello.com/b/57qX3bAA/project-1
 
-Link to Trello iFrame Mockup
-	http://imgur.com/vqTt0nr 
+Link to Trello wirefrom Mockup
+	http://imgur.com/5FEahFF
--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
+ var player1 = 0;
+ var player2 = 0; 
+ var player3 = 0;
 
-function randomScore(){
-	var randomScore = Math.floor(Math.random(x) *52 +1);
-	console.log("I got the score " + randomScore);	
 
-}
+ var $answer1 = $("#box1").click(function() {
+  	if (prompt("What is Question #1?") === "Kyle"){
+	    	alert("Correct");
+			document.getElementById("box1").background = "violet";
+	    	} else {
+	    		alert("NOOOOO");
+	    	}});
+ var $answer2 = $("#box2").click(function() {
+  	if (prompt("What is Question #2?") === "Kyle"){
+	    		alert("Correct");
+	    	} else {
+	    		alert("NOOOOO");
+	    	}});
+var $answer3 = $("#box3").click(function() {
+ 	if (prompt("What is Question #3?") === "Kyle"){
+	    		alert("Correct");
+	    	} else {
+	    		alert("NOOOOO");
+	    	}});
